bff094cdcd581be0dd79de5f324debd8a177b142
Gazaleev Timur <timsmailbox@inbox.ru>
2020-04-30 21:17:31 +0500

    Some refactoring

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-//TO-DO DEBUG!!!
-const fs = require("fs");
-const promisify = require("util").promisify;
-const fileReader = promisify(fs.readFile);
+const config = require("./config.json");
 const BotLogger = require("./logger");
 
-const loadConfiguration = async function (pathToConfig) {
-    const config = await fileReader(pathToConfig, "utf8");
-    if (!config)
-        throw new Error("Configuration file not read or not found!");
-
-    return JSON.parse(config);
-}
-
 const logInfo = async function () {
-    const config = await loadConfiguration();
     const logger = new BotLogger(process.env.TOKEN, config);
     logger.sendInfo("Test info message", config.recipients);
 }
 
 const logError = async function () {
-    const config = await loadConfiguration();
     const logger = new BotLogger(process.env.TOKEN, config);
     logger.sendError(new Error("Test error object!"), config.recipients);
 }
